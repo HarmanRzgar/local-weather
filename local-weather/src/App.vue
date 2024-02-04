986a4c2af6a9010c70ae29d713f8ffbc3834842f
Harman Rzgar <harmanrzgarmuhammad@gmail.com>
2024-02-04 16:23:00 +0300

    Initial Commit

---
+++ local-weather/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
+<SideNav/>
+<RouterView/>
+</div>
+</template>
+
+<script setup>
+import { RouterView } from "vue-router";
+import SideNav from "./components/sideNav.vue"
+</script>
+
+<style lang="scss" scoped></style>
